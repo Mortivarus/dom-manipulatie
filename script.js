d0a211128baa9ddeed063a52721a8d17e33d7637
Mortivarus <58182213+Mortivarus@users.noreply.github.com>
2021-02-17 13:00:24 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("para").onclick = function() {
+    fun(name, color)}
+
+    let name = "Henk"
+    let color = ["brown", "red", "white", "green", "purple"]
+    let random = 
+
+    function fun(naam, kleur) {  
+    document.getElementById("herp").innerHTML = `Hallo, ${naam}!`;  
+    document.getElementById("herp").style.color = kleur;  
+    document.getElementById("herp").style.backgroundColor = "yellow";  
+    document.getElementById("herp").style.fontSize = "25px";  
+    document.getElementById("herp").style.border = "4px solid red";   
+    }
+    
