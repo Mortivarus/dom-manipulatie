44307ee4f91c78dad553022ab0fd0f6bc63440ca
Mortivarus <58182213+Mortivarus@users.noreply.github.com>
2021-02-18 10:34:27 +0100

    Finished exercise

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-document.getElementById("para").onclick = function() {
-    fun(name, color)}
-
-    let name = "Henk"
-    let color = ["brown", "red", "white", "green", "purple"]
-    let random = 
-
-    function fun(naam, kleur) {  
+function fun() {
+    let naam = "Henk"
+    let color = ["brown", "red", "orange", "green", "purple"]
+    let random = Math.floor(Math.random()*color.length)
     document.getElementById("herp").innerHTML = `Hallo, ${naam}!`;  
-    document.getElementById("herp").style.color = kleur;  
-    document.getElementById("herp").style.backgroundColor = "yellow";  
-    document.getElementById("herp").style.fontSize = "25px";  
-    document.getElementById("herp").style.border = "4px solid red";   
+    document.getElementById("herp").style.color = color[random];
     }
-    
+
+document.getElementById("para").onclick = function() {
+    fun()
+}
