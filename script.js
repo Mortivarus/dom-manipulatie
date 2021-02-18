8bead870575c0c83ff5c2bc5d98c5bc3ea8899dc
Mortivarus <58182213+Mortivarus@users.noreply.github.com>
2021-02-18 11:39:18 +0100

    Added supervisor comments

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
+// CR :: Can you think of a better name for your function?
 function fun() {
-    let naam = "Henk"
-    let color = ["brown", "red", "orange", "green", "purple"]
-    let random = Math.floor(Math.random()*color.length)
-    document.getElementById("herp").innerHTML = `Hallo, ${naam}!`;  
+    // CR :: its better to use const and some variables should not be defined within the function
+    let naam = "Henk";
+    let color = ["brown", "red", "orange", "green", "purple"];  let random = Math.floor(Math.random() * color.length);
+    document.getElementById("herp").innerHTML = `Hallo, ${naam}!`;
     document.getElementById("herp").style.color = color[random];
-    }
-
-document.getElementById("para").onclick = function() {
-    fun()
-}
+  }// CR :: para is not really a proper name for an id, think of when being in a project group and someone else has to work with your code
+  // See if you can make this code work with 'addEventListener('click', fun);', to make it shorter
+  document.getElementById("para").addEventListener('click', fun)
